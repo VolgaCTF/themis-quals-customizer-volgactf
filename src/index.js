4ef2a045b2de18148eb46ab1ab8c05dff7199e8b
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-03-04 05:45:44 +0000

    Add favicons

--- src/index.js
+++ src/index.js
@@ -26,7 +26,35 @@ export default class DefaultCustomizer {
       },
       intro: {
         path: path.join(__dirname, '..', 'partials', 'intro.html')
+      },
+      favicons: {
+        path: path.join(__dirname, '..', 'partials', 'favicons.html')
       }
     }
+  }
+
+  getImages () {
+    return [
+      'apple-touch-icon-114x114.png',
+      'apple-touch-icon-120x120.png',
+      'apple-touch-icon-144x144.png',
+      'apple-touch-icon-152x152.png',
+      'apple-touch-icon-57x57.png',
+      'apple-touch-icon-60x60.png',
+      'apple-touch-icon-72x72.png',
+      'apple-touch-icon-76x76.png',
+      'favicon-128.png',
+      'favicon-16x16.png',
+      'favicon-196x196.png',
+      'favicon-32x32.png',
+      'favicon-96x96.png',
+      'favicon.ico',
+      'mstile-144x144.png',
+      'mstile-150x150.png',
+      'mstile-310x150.png',
+      'mstile-70x70.png'
+    ].map((imagePath) => {
+      return path.join(__dirname, '..', 'images', imagePath)
+    })
   }
 }
